39e69c248028e6ab9574a355c8ad82f1977f37ba
felipe de la vega <felip@MacBook-Air.local>
2025-04-04 17:01:03 -0300

    fix: muevo carpeta components a src y ajusto import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import PetEcommerceMVP from "../../components/pets/PetEcommerceMVP";
+import PetEcommerceMVP from "../components/pets/PetEcommerceMVP";
 
 export default function Home() {
   return <PetEcommerceMVP />;
