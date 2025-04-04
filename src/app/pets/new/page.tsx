9e3ce145afe2d6bff8c6e751468a18812bafe1eb
felipe de la vega <felip@MacBook-Air.local>
2025-04-04 16:54:16 -0300

    test: agrego contenido visible para probar MVP

--- src/app/pets/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import PetWizard from '@/components/pets/PetWizard'
-
-export default function NewPetPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <PetWizard />
-    </div>
-  )
-} 
